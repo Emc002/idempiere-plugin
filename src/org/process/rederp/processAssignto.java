6a1b91205ae2801b72c52407f1654fb503a6b5c1
alifnafis <alifnafisalghany@gmail.com>
2023-05-19 23:53:35 +0700

    process-ui

--- src/org/process/rederp/processAssignto.java
+++ src/org/process/rederp/processAssignto.java
@@ -1,5 +1,6 @@
 package org.process.rederp;
 
+import org.adempiere.util.Callback;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.DB;
@@ -27,19 +28,65 @@ public class processAssignto extends SvrProcess {
 		String tableName = "a_asset";
 		String columnName = "ad_user_id";
 		
+		final StringBuilder yesNo = new StringBuilder();
+		final Object lock = new Object();
+		processUI.ask("Yes or No?", new Callback<Boolean>() {
+			
+			
+
+			@Override
+			public void onCallback(Boolean result) {
+				// TODO Auto-generated method stub
+				yesNo.append(result);
+				synchronized (lock) {
+                    lock.notify();
+                }
+				
+			}
+		});
+		
+        synchronized (lock) {
+            while (yesNo.length() == 0) {
+                lock.wait();
+            }
+        }
+		
+		final StringBuilder string = new StringBuilder();
+		final Object stringLock = new Object();
+		
+		processUI.askForInput("Please Enter a String", new Callback<String>() {
+
+			@Override
+			public void onCallback(String result) {
+				// TODO Auto-generated method stub
+				string.append(result);
+				synchronized (stringLock) {
+					stringLock.notify();
+                }
+			}
+		});
+		
+        synchronized (stringLock) {
+            while (string.length() == 0) {
+                stringLock.wait();
+            }
+        }
+        
 		int updatedRows = DB.executeUpdate(
-			"UPDATE " + tableName +
-			" SET " + columnName + " = ?" +
-			" WHERE " + "a_asset_id" + " = " + assetId,
-			userId,
-			get_TrxName()
-		);
+				"UPDATE " + tableName +
+				" SET " + columnName + " = ?" +
+				" WHERE " + "a_asset_id" + " = " + assetId,
+				userId,
+				get_TrxName()
+			);
+			
+			if (updatedRows == -1) {
+				
+				throw new Exception("Failed to update the value of " + columnName + " in table " + tableName);
+			}
 		
-		if (updatedRows == -1) {
-			throw new Exception("Failed to update the value of " + columnName + " in table " + tableName);
-		}
-		
-		return "Updated " + updatedRows + " rows";
+			
+        return "Updated " + updatedRows + " rows. Note:" + string;
 	}
 }
 
